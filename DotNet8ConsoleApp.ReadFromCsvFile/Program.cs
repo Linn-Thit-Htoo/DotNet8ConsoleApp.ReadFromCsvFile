90bae84a9614d7f5b6f61c00913f61b238108756
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-27 13:45:42 +0630

    Add project files.

---
+++ DotNet8ConsoleApp.ReadFromCsvFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using CsvHelper;
+using DotNet8ConsoleApp.ReadFromCsvFile.Models;
+
+using (var reader = new StreamReader("Data/admin.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var lst = csv.GetRecords<CsvModel>().ToList();
+    foreach(var record in lst)
+    {
+        Console.WriteLine($"ID: {record.ID}");
+        Console.WriteLine($"IDName {record.Name}");
+        Console.WriteLine($"Email: {record.Email}");
+        Console.WriteLine("-------------------");
+    }
+}
+
+Console.ReadKey();
