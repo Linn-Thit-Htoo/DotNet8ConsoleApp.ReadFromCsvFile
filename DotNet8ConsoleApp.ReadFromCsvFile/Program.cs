e7f165762846ee05a36a3d6258a78096333f8f24
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-27 13:48:56 +0630

    -

--- DotNet8ConsoleApp.ReadFromCsvFile/Program.cs
+++ DotNet8ConsoleApp.ReadFromCsvFile/Program.cs
@@ -1,14 +1,14 @@
-﻿using System.Formats.Asn1;
+﻿using System;
+using System.Formats.Asn1;
 using System.Globalization;
-using System;
 using CsvHelper;
 using DotNet8ConsoleApp.ReadFromCsvFile.Models;
 
-using (var reader = new StreamReader("Data/admin.csv"))
+using (var reader = new StreamReader("Data/data.csv"))
 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
 {
     var lst = csv.GetRecords<CsvModel>().ToList();
-    foreach(var record in lst)
+    foreach (var record in lst)
     {
         Console.WriteLine($"ID: {record.ID}");
         Console.WriteLine($"IDName {record.Name}");
